ab4b92359aa86ad8206adfa3a8d8a0e32535e9fa
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-05-15 19:04:56 +0200

    Initial import from cpp autotools lib example

---
+++ include/toollib.h
@@ -0,0 +1,11 @@
+#ifndef TOOLLIB_H_
+#define TOOLLIB_H_
+
+#include "toollib/print.h"
+
+#include <iostream>
+using namespace std;
+
+int toollib(string input);
+
+#endif
---
+++ src/toollib/print.cpp
@@ -0,0 +1,10 @@
+#include <toollib.h>
+
+#include <iostream>
+using namespace std;
+
+int print(string input){
+	cout << input << endl;
+	return 0;
+}
+
